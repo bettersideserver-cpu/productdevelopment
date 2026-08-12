8815b40ae45cbd476185cfa7da6eb508f9322bdd
betterside <bettersideserver@gmail.com>
2026-08-12 12:14:32 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 export const USE_SUPABASE = true;
 
 export const SUPABASE_URL =
-    'https://fqsvdyfrwdqidaktqwzi.supabase.co';
+    'https://fqsvdyfrwdqidakqtwzi.supabase.co/rest/v1/';
 
 export const SUPABASE_PUBLISHABLE_KEY =
     'sb_publishable_2hoOq8LdqQYgeNhxctYMPQ_gd0oqByV';
